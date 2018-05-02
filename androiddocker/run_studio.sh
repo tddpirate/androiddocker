2ac81e73e13c386575f8b244c2d0fa481dcc1f83
Omer Zak <w1@zak.co.il>
2018-05-02 21:14:55 +0300

    Updated Dockerfile for: - shorter list of packages to manually install. - use run_studio.sh to allow Android Studio upgrade by closing it.

---
+++ androiddocker/run_studio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+########################################################################
+# Sometimes you have to exit Android Studio to complete its upgrade.
+# In such a case, you want the container to keep running and perform
+# 'docker exec' to run a shell inside it.
+#
+# To accomplish this, touch the file 'upgrade' in your
+# $CONTAINER_PROJECTS directory.
+########################################################################
+echo To keep container running after closing Android Studio for
+echo upgrading it, perform: 'touch androidprojects/upgrade'
+echo before closing Android Studio.
+android-studio/bin/studio.sh
+if [ -e projects/upgrade ] ; then
+  # The user wishes to upgrade Android Studio outside of it.
+  rm projects/upgrade
+  tail -f /dev/null
+fi
+# End of run_studio.sh

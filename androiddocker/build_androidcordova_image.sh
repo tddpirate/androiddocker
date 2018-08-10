eb49da2233c327ab23ffb5ffbd2e9fb148d3ad89
Omer Zak <w1@zak.co.il>
2018-08-10 16:21:48 +0300

    The Cordova build script is now conscious of the Android Studio image tag.

--- androiddocker/build_androidcordova_image.sh
+++ androiddocker/build_androidcordova_image.sh
@@ -25,17 +25,32 @@ if [ ! -e $CONTAINER_HOMEDIR/home ] ; then
 fi
 
 # Ensure that we have a tddpirate/androidstudio image
-docker images --format "{{.Repository}}"tddpirate/androidstudio | grep -q tddpirate/androidstudio
+docker images --format "{{.Repository}}" | grep -q tddpirate/androidstudio
 STATUS=$?
 if [ $STATUS != 0 ]; then
-  echo "You need the tddpirate/androidstudio image and it does not exist"
+  echo "You need a tddpirate/androidstudio image and no such image exists."
   exit 1
 fi
 
+echo The following images exist:
+docker images --format "{{.Repository}}:{{.Tag}}" | grep tddpirate/androidstudio
+IMAGE_LF=`docker images --format "{{.Repository}}:{{.Tag}}" | grep tddpirate/androidstudio | tail -1`
+IMAGE="${IMAGE_LF//[$'\n']}"
+TAG="${IMAGE##*:}"
+
+echo "Confirm the Android Studio image upon which you want to base the new image:"
+read -ei "$IMAGE" ANDROID_STUDIO_IMAGE
+
 # Actually build the image
-echo "Enter a name for the new image (do not forget to add a tag):"
-read -ei "tddpirate/androidcordova" IMAGE_NAME
-docker build -f $DOCKERFILE -t $IMAGE_NAME .
+echo "Enter a name for the new image:"
+read -ei "tddpirate/androidcordova:${TAG}" NEW_IMAGE_NAME
+
+echo DOCKERFILE=${DOCKERFILE}
+echo NEW_IMAGE_NAME=${NEW_IMAGE_NAME}
+echo ANDROID_STUDIO_IMAGE=${ANDROID_STUDIO_IMAGE}
+echo "Starting to build the new image."
+
+docker build -f $DOCKERFILE -t $NEW_IMAGE_NAME --build-arg ANDROID_STUDIO_IMAGE=${ANDROID_STUDIO_IMAGE} .
 
 echo "Running the image, select a name for the container:"
 read -ei "android-cordova" CONTAINER_NAME
@@ -46,6 +61,6 @@ docker run -ti \
            --privileged -v /dev/bus/usb:/dev/bus/usb \
            -e XAUTHORITY=/AndroidStudio/home/.docker.xauth \
            --name=$CONTAINER_NAME \
-           $IMAGE_NAME
+           $NEW_IMAGE_NAME
 
 echo "To continue to use the container, restart it."
